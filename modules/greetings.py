2be68a543ed4cd11a8f24321b5a836e340eac957
HLAldred <haileylynn.aldred@lskysd.ca>
2025-04-07 09:20:26 -0600

    Add files via upload

---
+++ modules/greetings.py
@@ -0,0 +1,20 @@
+#--------------
+#FP3-F01-Libraries-Greetings
+#Hailey-lynn Aldred
+#01 April 2025
+#Modules --> External Library 1 
+#Simple
+#--------------
+
+#----Dictionaries----#
+music = {'Ghost':'Future is a Foreign Land', 'Queen':'Radio Ga Ga', 'Pearl Jam':'Just Breathe'}
+
+#----Functions----#
+def name(x): #allows for an argument to be passed into the function
+    print("Hello, my name is", x)
+    print("What is your name?")
+    user_name = input("> ")
+    print("It's nice to meet you,", user_name) #inserts the user variable
+
+genre = "One of my favourite genres is hard rock." #variable.
+    
---
+++ modules/view.py
@@ -0,0 +1,17 @@
+#--------------
+#FP3-F01-Libraries-Guess
+#Hailey-lynn Aldred
+#02 April 2025
+#Modules --> External Library 2
+#return value
+#--------------
+
+numbers = [3, 7, 9, 13, 21]
+
+def look(x):
+    if x in numbers:
+        return "This number is aesthetically pleasing."
+    else:
+        return "This number is not aesthetically pleasing."
+    
+cool = "Isn't it cool how we can use the modules that we create?"
